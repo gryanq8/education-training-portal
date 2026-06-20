c8f106afd1517ffbf1b7e05f606dbbaaba3e124a
gryanq8 <gryanq8@icloud.com>
2026-06-20 18:20:56 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,9 +1,10 @@
-const SUPABASE_URL = "https://yezmbhwfemardwgtnroz.supabase.co";
+const SUPABASE_URL =
+"https://yezmbhwfemardwgtnroz.supabase.co";
 
 const SUPABASE_ANON_KEY =
 "sb_publishable_Hb1ySpsX74hdVBOB0QPCIQ_W6SqFQuo";
 
-const supabaseClient = supabase.createClient(
+window.supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
